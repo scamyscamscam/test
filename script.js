970cbc86531b511e75fd385bb82e0e252ec2ebec
carlos <celik.jaka03@gmail.com>
2022-08-10 21:38:06 +0200

    final?

---
+++ script.js
@@ -0,0 +1,20 @@
+let y = 0;
+
+
+function pls(id){
+    let counter = document.getElementById(id).innerHTML;
+    let stevilka = document.getElementById(id);
+    x = Number(counter);
+    x++;
+    stevilka.innerHTML = x;
+    return y = x;
+}
+
+function igh(id){
+    let counter = document.getElementById(id).innerHTML;
+    let stevilka = document.getElementById(id);
+    x = Number(counter);
+    x--;
+    stevilka.innerHTML = x;
+    return y = x;
+}
